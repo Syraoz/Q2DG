136508d9ab37a3d449e16157f92398f6d5c8b8f5
KinixKX <KinixKX@gmail.com>
2019-06-11 00:26:28 -0500

    Added program window

---
+++ KinixPGT/Assets/Scripts/GUI/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MainWindow : EditorWindow
+{
+
+    [MenuItem("Window/KinixPGT")]
+
+    public static void ShowWindow()
+    {
+        GetWindow<MainWindow>("KinixPGT");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Welcome to the prototype version of PGNT!");
+
+        if (GUILayout.Button("Generate")){
+
+        }
+    }
+
+
+}
